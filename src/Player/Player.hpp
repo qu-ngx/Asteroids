eb45320928a87e27c21a951bd67c081efa19a460
qu-ngx <quang13012005@gmail.com>
2024-03-30 16:59:32 -0400

    refactored Player.hpp

--- src/Player/Player.hpp
+++ src/Player/Player.hpp
@@ -1,46 +1,46 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include "lib/raylib.h"
 #include "lib/raymath.h"
+#define MAX_PLAYER_VELOCITY 150
+
 class Player
 {
-private:
 public:
     Vector2 position;
     float rotation;
     float acceleration;
     Vector2 velocity;
 
-    // void init_player()
-    // {
-    //     Player player;
-    //     player.position.x = 400;
-    //     player.position.y = 300;
-    //     player.rotation = PI / 2.0;
+    Player()
+    {
+        position.x = 400;
+        position.y = 300;
+        rotation = PI / 2.0;
 
-    //     player.acceleration = 100.0;
-    //     player.velocity = Vector2Zero();
-    // }
+        acceleration = 100.0;
+        velocity = Vector2Zero();
+    }
 
-    // void draw_player()
-    // {
-    //     Player player;
-    //     Vector2 top = {0, -10};
-    //     Vector2 left = {-10, 10};
-    //     Vector2 right = {10, 10};
+    void draw_player()
+    {
+        Vector2 top = {0, -10};
+        Vector2 left = {-10, 10};
+        Vector2 right = {10, 10};
 
-    //     top = Vector2Rotate(top, player.rotation);
-    //     left = Vector2Rotate(left, player.rotation);
-    //     right = Vector2Rotate(right, player.rotation);
+        top = Vector2Rotate(top, rotation);
+        left = Vector2Rotate(left, rotation);
+        right = Vector2Rotate(right, rotation);
 
-    //     Vector2 t = Vector2Add(top, player.position);
-    //     Vector2 l = Vector2Add(left, player.position);
-    //     Vector2 r = Vector2Add(right, player.position);
+        Vector2 t = Vector2Add(top, position);
+        Vector2 l = Vector2Add(left, position);
+        Vector2 r = Vector2Add(right, position);
 
-    //     DrawLineV(t, l, GREEN);
-    //     DrawLineV(t, r, GREEN);
-    //     DrawLineV(l, r, GREEN);
-    // }
+        DrawLineV(t, l, GREEN);
+        DrawLineV(t, r, GREEN);
+        DrawLineV(l, r, GREEN);
+    }
 };
 
 #endif
